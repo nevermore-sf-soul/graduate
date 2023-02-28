eb7498570f3db83e1e1dca8cbc54e90ca12d69b5
nevermore-sf-soul <jwwu_1@163.com>
2023-02-28 21:41:26 +0800

    z

---
+++ src/main/java/org/workflowsim/algorithms/iheft.java
@@ -0,0 +1,18 @@
+package org.workflowsim.algorithms;
+
+import org.workflowsim.Environment;
+import org.workflowsim.Task;
+import org.workflowsim.Vm;
+
+public class iheft {
+    Environment environment;
+    public iheft(Environment environment){this.environment=environment;}
+    public void execute(Task i)
+    {
+        if(i.getPrivacy_level()==1)
+        {
+
+        }
+    }
+
+}
